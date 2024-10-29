98dc02ae23da77c290c09f0e0b4ac600b5d69080
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-10-29 23:31:55 +0900

    HTML,CSS作成

---
+++ js/main.js
@@ -0,0 +1,10 @@
+'use strict'
+{
+  const form = document.querySelector('form');
+  const textarea = document.querySelector('textarea');
+
+  form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+  
+  });
+}
